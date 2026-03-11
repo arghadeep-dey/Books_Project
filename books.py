325616e4073a531a6d823cbb3bc46aec3e22b3e9
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-03-11 23:55:47 +0530

    Adding GET, POST, PUT and Delete API

--- books.py
+++ books.py
@@ -1,16 +1,79 @@
-# This is a sample Python script.
+from fastapi import Body,FastAPI
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
+BOOKS = [
+    {"title": "My first book",
+    "author": "Author1",
+     "category":"Maths"},
+    {"title": "My second book",
+    "author": "Author2",
+     "category":"Maths"},
+    {"title": "My third book",
+    "author": "Author3",
+     "category":"Physics"},
+    {"title": "My fourth book",
+    "author": "Author4",
+     "category":"Physics"},
+    {"title": "My fifth book",
+     "author": "Author5",
+     "category":"Maths"},
+    {"title": "My sixth book",
+     "author": "Author2",
+     "category":"Computers"}
+]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
 
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
+    return "No Books Found"
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/books/{book_author}/")
+async def read_author_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        print (book_author)
+        print (book.get('author'))
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+@app.put("/books/update_book")
+async def update_book(updated_book = Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get("title").casefold() == updated_book.get("title").casefold():
+            BOOKS[i] = updated_book
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get("title").casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
+
+#All Books for a specific author
+@app.get("/books/byauthor/{author}")
+async def read_by_author(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get("author").casefold() == author.casefold():
+            books_to_return.append(book)
+    return books_to_return
