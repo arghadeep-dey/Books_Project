9238e43c6557438980a4af31a5634d2433057627
Arghadeep Dey <arghadeepdey.official.2001@gmail.com>
2026-04-11 23:44:20 +0530

    Authentication

--- ToRead/routers/auth.py
+++ ToRead/routers/auth.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 try:
     from ..database import SessionLocal
@@ -33,6 +34,15 @@ def get_db():
         db.close()
 
 db_dependencies = Annotated[Session, Depends(get_db)]
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.password):
+        return False
+    return True
+
 #Create New To Read
 @router.post("/auth/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependencies,create_user_request: CreateUserRequest):
@@ -47,3 +57,12 @@ async def create_user(db: db_dependencies,create_user_request: CreateUserRequest
     )
     db.add(create_user_model)
     db.commit()
+
+
+@router.post("/token/", status_code=status.HTTP_201_CREATED)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                      db: db_dependencies):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Failed authentication"
+    return "Successful authentication"
